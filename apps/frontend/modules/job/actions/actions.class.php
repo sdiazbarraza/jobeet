858013f4bf0bae581e5625a5d824ec011e92e299
Sebastian <simpdev7@gmail.com>
2017-11-27 01:57:30 -0300

    Day 17 complete

---
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * job actions.
+ *
+ * @package    jobeet
+ * @subpackage job
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class jobActions extends sfActions
+{
+    public function executeSearch(sfWebRequest $request)
+    {
+        $this->forwardUnless($query = $request->getParameter('query'), 'job', 'index');
+
+        $this->jobs = Doctrine_Core::getTable('JobeetJob') ->getForLuceneQuery($query);
+    }
+  public function executeIndex(sfWebRequest $request)
+  {
+      $this->categories = Doctrine_Core::getTable('JobeetCategory')->getWithJobs();
+  }
+
+    public function executeShow(sfWebRequest $request)
+    {
+
+        $this->job = $this->getRoute()->getObject();
+
+        $this->getUser()->addJobToHistory($this->job);
+    }
+    public function executeNew(sfWebRequest $request)
+    {
+
+        $job = new JobeetJob();
+        $job->setType('full-time');
+        $this->form = new JobeetJobForm();
+    }
+
+    public function executeCreate(sfWebRequest $request)
+    {
+        $this->form = new JobeetJobForm();
+        $this->processForm($request, $this->form);
+        $this->setTemplate('new');
+    }
+
+    public function executeEdit(sfWebRequest $request)
+    {
+        $this->form = new JobeetJobForm($this->getRoute()->getObject());
+    }
+
+    public function executeUpdate(sfWebRequest $request)
+    {
+        $this->form = new JobeetJobForm($this->getRoute()->getObject());
+        $this->processForm($request, $this->form);
+        $this->setTemplate('edit');
+    }
+
+    public function executePublish(sfWebRequest $request)
+    {
+        $request->checkCSRFProtection();
+
+        $job = $this->getRoute()->getObject();
+        $job->publish();
+
+        $this->getUser()->setFlash('notice', sprintf('Your job is now online for %s days.', sfConfig::get('app_active_days')));
+
+        $this->redirect($this->generateUrl('job_show_user', $job));
+    }
+    public function executeDelete(sfWebRequest $request)
+    {
+        $request->checkCSRFProtection();
+
+        $job = $this->getRoute()->getObject();
+        $job->delete();
+
+        $this->redirect('job/index');
+    }
+    public function executeExtend(sfWebRequest $request)
+    {
+        $request->checkCSRFProtection();
+
+        $job = $this->getRoute()->getObject();
+        $this->forward404Unless($job->extend());
+
+        $this->getUser()->setFlash('notice', sprintf('Your job validity has been extended until %s.', $job->getDateTimeObject('expires_at')->format('m/d/Y')));
+
+        $this->redirect($this->generateUrl('job_show_user', $job));
+    }
+
+    protected function processForm(sfWebRequest $request, sfForm $form)
+    {
+        $form->bind(
+            $request->getParameter($form->getName()),
+            $request->getFiles($form->getName())
+        );
+
+        if ($form->isValid())
+        {
+            $job = $form->save();
+
+            $this->redirect($this->generateUrl('job_show', $job));
+        }
+    }
+
+
+
+}
